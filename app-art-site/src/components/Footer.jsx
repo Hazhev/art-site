daed4697bb75f8167b9668056a0a5293a79c0969
Hazhev <maclarengtr@mail.ru>
2022-09-22 19:34:24 +0300

    Footer

---
+++ app-art-site/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../Styles/Footer.css'
+import footerLogo from '../image/footerLogo.svg'
+
+const Footer = () => {
+    return(
+        <div className="wrapper-footer">
+            <div className="container-footer">
+                <img src={footerLogo} alt="Лого" id="footerLogo"/>
+                <p className="footer-text">2022. Все права защищены</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Footer;
