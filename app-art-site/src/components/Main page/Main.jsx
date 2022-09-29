dbf9c17f555a9791fb2768816d93659a9b025035
Hazhev <maclarengtr@mail.ru>
2022-09-29 20:10:03 +0300

    desktop

--- app-art-site/src/components/Main page/Main.jsx
+++ app-art-site/src/components/Main page/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../../Styles/Main.css'
+import ArtistsMini from "./ArtistsMini";
 import Category from "./Category";
 import Collectioners from "./Collectioners";
 import Slider2 from "./Slider2";
@@ -15,6 +16,7 @@ const Main = () => {
                     <Category/>
                     <Collectioners/>
                 </div>
+                <ArtistsMini/>
             </div>
         </div>
     )
