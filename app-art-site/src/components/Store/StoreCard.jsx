0f7b10789626a0f133c9e1ff80d1a80b3f8efb99
gingerFury7 <slavka2000@mail.ru>
2022-10-05 00:03:56 +0600

    store card from 430px to 767px

--- app-art-site/src/components/Store/StoreCard.jsx
+++ app-art-site/src/components/Store/StoreCard.jsx
@@ -30,6 +30,26 @@ const StoreCard = () => {
         <img className="store-card-btn" src={LikeBtn} />
         <img className="store-card-btn" src={AddBtn} />
       </div>
+      <div className="store-card-info-mobile-container">
+        <div className="store-card-title">Лодка-водка</div>
+        <div className="store-card-subtitle-line">
+          <div className="store-card-subtitle-left">
+            <div className="store-card-subtitle">
+              Оригинал картины, 80 x 60 см
+            </div>
+            <div className="store-card-author">Виктор Пономаренко</div>
+          </div>
+          <img className="store-card-btn" src={LikeBtn} />
+        </div>
+        <div className="store-card-price-line">
+          <div className="store-card-price">144 000 руб.</div>
+          <img className="store-card-btn" src={AddBtn} />
+        </div>
+        <div className="store-card-btn-line">
+          <img className="store-card-btn" src={LikeBtn} />
+          <img className="store-card-btn" src={AddBtn} />
+        </div>
+      </div>
     </div>
   );
 };
