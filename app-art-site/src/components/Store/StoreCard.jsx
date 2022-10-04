d96b7e54b3de9cac270447c79a09f230b59c83ab
gingerFury7 <slavka2000@mail.ru>
2022-10-04 22:02:55 +0600

    store desktop

--- app-art-site/src/components/Store/StoreCard.jsx
+++ app-art-site/src/components/Store/StoreCard.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "../../Styles/Store.css";
 import img from "../../image/auctions-img.png";
+import investRating from "../../image/invest_rating.svg";
+import LikeBtn from "../../image/favourite-store-art.svg";
+import AddBtn from "../../image/add-btn.svg";
 
 const StoreCard = () => {
   return (
     <div className="store-card">
       <img className="store-img" src={img} />
+      <img className="store-invest-rating" src={investRating} />
+      <div className="store-card-title">Лодка-водка</div>
+      <div className="store-card-subtitle-line">
+        <div className="store-card-subtitle-left">
+          <div className="store-card-subtitle">
+            Оригинал картины, 80 x 60 см
+          </div>
+          <div className="store-card-author">Виктор Пономаренко</div>
+        </div>
+        <img className="store-card-btn" src={LikeBtn} />
+      </div>
+      <div className="store-card-price-line">
+        <div className="store-card-price">144 000 руб.</div>
+        <img className="store-card-btn" src={AddBtn} />
+      </div>
     </div>
   );
 };
